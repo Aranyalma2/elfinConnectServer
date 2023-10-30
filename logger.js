117dd7ebbe6226a87c9bdc8fc6ef6d8a8aa1dac6
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2023-10-30 23:54:34 +0100

    deviceToUser
    Connect device to user fixed

--- logger.js
+++ logger.js
@@ -1,19 +1,17 @@
-const winston = require('winston');
+const winston = require("winston");
 
 // Define a custom log format with colors
 const logFormat = winston.format.combine(
-  //winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-  winston.format.colorize(),
-  winston.format.simple()
+	//winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+	winston.format.colorize(),
+	winston.format.simple(),
 );
 
 // Create a logger with the console transport
 const logger = winston.createLogger({
-  level: 'verbose', // Set your desired log level
-  format: logFormat,
-  transports: [
-    new winston.transports.Console()
-  ]
+	level: "silly", // Set your desired log level
+	format: logFormat,
+	transports: [new winston.transports.Console()],
 });
 
 module.exports = logger;
