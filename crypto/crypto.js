f9fdb4a37d8b3c27e49ad7174e41850a06d5d6f2
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2024-01-23 22:47:30 +0100

    aes padding change
    Change AES block padding cahnged to NoPadding to ZeroPadding

--- crypto/crypto.js
+++ crypto/crypto.js
@@ -9,7 +9,7 @@ const key = CryptoJS.enc.Utf8.parse(keyUTF8);
 function decryptAESCBC(encryptedText){
     let decryptedText = CryptoJS.AES.decrypt(encryptedText, key, {
     mode: CryptoJS.mode.CBC,
-    padding: CryptoJS.pad.NoPadding,
+    padding: CryptoJS.pad.ZeroPadding,
     iv: key
     });
     return decryptedText.toString(CryptoJS.enc.Utf8);
@@ -18,10 +18,10 @@ function decryptAESCBC(encryptedText){
 function encryptAESCBC(messageText){
     let encryptedText = CryptoJS.AES.encrypt(messageText, key, {
     mode: CryptoJS.mode.CBC,
-    padding: CryptoJS.pad.NoPadding,
+    padding: CryptoJS.pad.ZeroPadding,
     iv: key
     });
-    return encryptedText;
+    return Buffer.from(encryptedText.ciphertext.toString(CryptoJS.enc.Hex), 'hex');
 }
 
 module.exports = { decryptAESCBC, encryptAESCBC};
