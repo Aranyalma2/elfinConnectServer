bac09fb01b69ab557c910ffc5c26e9f6f466b870
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2024-02-11 21:20:20 +0100

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const logger = require("../logger");
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect("mongodb://10.101.2.4:27017/test");
+    await mongoose.connect("mongodb://database:27017/test");
     logger.info("DB Connected!");
   } catch (error) {
     logger.error("DB Connection Error:", error);
@@ -12,25 +12,25 @@ const connectToDatabase = async () => {
 
 // Define the Device schema and model
 const deviceSchema = new mongoose.Schema({
-    hostName: String,
-    macAddress: String,
-    lastSeenDate: Date,
+  hostName: String,
+  macAddress: String,
+  lastSeenDate: Date,
 });
 
 const Device = mongoose.model("Device", deviceSchema);
 
 // Define the User schema and model
 const userSchema = new mongoose.Schema({
-    uuid: String,
-    name: String,
-    password: String,
-    allDevices: [{ type: mongoose.Schema.Types.ObjectId, ref: "Device" }],
+  uuid: String,
+  name: String,
+  password: String,
+  allDevices: [{ type: mongoose.Schema.Types.ObjectId, ref: "Device" }],
 });
 
 const User = mongoose.model("User", userSchema);
 
 module.exports = {
-    connectToDatabase,
-    Device,
-    User,
+  connectToDatabase,
+  Device,
+  User,
 };
