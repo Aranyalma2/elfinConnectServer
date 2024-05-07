54e7b53f3fda20cf664c2ed26455321198129d34
Aranyalma2 <40268481+Aranyalma2@users.noreply.github.com>
2024-05-07 14:15:24 +0200

    payload encode fix
    Fixed payload encoding bug.
    - Send out and got payload mismatch

--- endpoint/device.js
+++ endpoint/device.js
@@ -2,71 +2,73 @@ const logger = require("../logger");
 
 let activeDevices = {};
 
-function createActiveDevice(ownerUuid, hostName, macAddress, clientSocket){
-    const lastSeenDate = new Date();
-    const device = {
-        ownerUuid,
-        hostName,
-        macAddress,
-        clientSocket,
-        lastSeenDate,
-        // Define the method to compare the device with another one
-        equals: function (otherDevice) {
-            return (
-                this.ownerUuid === otherDevice.ownerUuid &&
-                this.macAddress === otherDevice.macAddress
-            );
-        },
-    };
-    return device;
+function createActiveDevice(ownerUuid, hostName, macAddress, clientSocket) {
+	const lastSeenDate = new Date();
+	const device = {
+		ownerUuid,
+		hostName,
+		macAddress,
+		clientSocket,
+		lastSeenDate,
+		// Define the method to compare the device with another one
+		equals: function (otherDevice) {
+			return this.ownerUuid === otherDevice.ownerUuid && this.macAddress === otherDevice.macAddress;
+		},
+	};
+	return device;
 }
 
 //Create key for collection
-function getKey(ownerUuid, macAddress){
-    return `${ownerUuid}-${macAddress}`;
+function getKey(ownerUuid, macAddress) {
+	return `${ownerUuid}-${macAddress}`;
 }
 
 //Check device is in the list
 function deviceExists(key) {
-    return activeDevices[key] !== undefined;
+	return activeDevices[key] !== undefined;
 }
 
 // Function to add or update an active device
 function addOrUpdateDevice(device) {
-    activeDevices[getKey(device.ownerUuid, device.macAddress)] = device;
+	const key = getKey(device.ownerUuid, device.macAddress);
+	if (device.hostName === null && deviceExists(key)) {
+		device.hostName = getDevice(key).hostName;
+	} else if (device.hostName === null) {
+		logger.debug(`Device hostname is null, (data before first heartbeat), fill it temporarly.| User: ${device.ownerUuid} | Device: ${device.macAddress}`);
+		device.hostName = "Unknown";
+	}
+	activeDevices[key] = device;
 }
 
 // Function to get an device in active list by key
 function getDevice(key) {
-    const device = activeDevices[key];
-    if (device === undefined) {
-        throw new Error(`Device is not exists: User: ${key.split("-")[0]} | Device: ${key.split("-")[1]}`);
-    }
-    return device;
+	const device = activeDevices[key];
+	if (device === undefined) {
+		throw new Error(`Device is not exists: User: ${key.split("-")[0]} | Device: ${key.split("-")[1]}`);
+	}
+	return device;
 }
 
 // Function to check if a device is online based on the lastSeenDate
 function isOnline(key) {
-    const device = activeDevices[key];
-    //Check device is exists in activity list
-    if (device === undefined) {
-        logger.warn(
-            `Device is not exists in active client list, impossibly to be online | User: ${key.split("-")[0]} | Device: ${key.split("-")[1]}`
-        );
-        return false;
-    }
-    //Return true if lastSeenDate updated less then a minutes ago
-    return device.lastSeenDate > new Date(Date.now() - 60000);
+	const device = activeDevices[key];
+	//Check device is exists in activity list
+	if (device === undefined) {
+		logger.warn(`Device is not exists in active client list, impossibly to be online | User: ${key.split("-")[0]} | Device: ${key.split("-")[1]}`);
+		return false;
+	}
+	//Return true if lastSeenDate updated less then a minutes ago
+	return device.lastSeenDate > new Date(Date.now() - 60000);
 }
 
 //Active device mannger object
 const activeDeviceMannger = {
-    getKey,
-    deviceExists,
-    createActiveDevice,
-    addOrUpdateDevice,
-    getDevice,
-    isOnline
+	getKey,
+	deviceExists,
+	createActiveDevice,
+	addOrUpdateDevice,
+	getDevice,
+	isOnline,
 };
 
 module.exports = activeDeviceMannger;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const net = require('net');
+const net = require("net");
 const tunnelDataHandler = require("./tunnelDataHandler");
 const logger = require("./logger");
-const bridge = require("./bridge/connection") 
+const bridge = require("./bridge/connection");
 
 //Persistance Collections
 const database = require("./database/db.js");
@@ -10,60 +10,54 @@ const serverPort = 3001;
 
 database.connectToDatabase();
 
-function onError(clientSocket, error){
-    logger.warn(`Client socket error: ${error}`);
-    logger.warn(`${clientSocket.remoteAddress}:${clientSocket.remotePort}`);
-    try{
-      bridge.deleteSocketConnectionBySocket(clientSocket);
-    } catch(error){
-      logger.warn(error);
-    }
+function onError(clientSocket, error) {
+	logger.warn(`Client socket error: ${error}`);
+	logger.warn(`${clientSocket.remoteAddress}:${clientSocket.remotePort}`);
+	try {
+		bridge.deleteSocketConnectionBySocket(clientSocket);
+	} catch (error) {
+		logger.warn(error);
+	}
 }
 
-function onEnd(clientSocket){
-    logger.verbose('Client socket closed.');
-    logger.verbose(`${clientSocket.remoteAddress}:${clientSocket.remotePort}`);
-    try{
-      bridge.deleteSocketConnectionBySocket(clientSocket);
-    } catch(error){
-      logger.warn(error);
-    }
+function onEnd(clientSocket) {
+	logger.verbose("Client socket closed.");
+	logger.verbose(`${clientSocket.remoteAddress}:${clientSocket.remotePort}`);
+	try {
+		bridge.deleteSocketConnectionBySocket(clientSocket);
+	} catch (error) {
+		logger.warn(error);
+	}
 }
 
-function onData(clientSocket, data){
-      try{
-
-        tunnelDataHandler(clientSocket, data.toString());
-  
-      } catch(error){
-
-        logger.warn(`Message process error: ${error}`);
-
-    }
+function onData(clientSocket, data) {
+	try {
+		tunnelDataHandler(clientSocket, data);
+	} catch (error) {
+		logger.warn(`Message process error: ${error}`);
+	}
 }
 
 // Create a TCP server that listens for incoming connections
 const planeTcpServer = net.createServer((clientSocket) => {
+	logger.info(`Device connected: ${clientSocket.remoteAddress}:${clientSocket.remotePort}`);
 
-  logger.info(`Device connected: ${clientSocket.remoteAddress}:${clientSocket.remotePort}`);
+	// Handle errors, remove socket from bridge if it is exists
+	clientSocket.on("error", (error) => {
+		onError(clientSocket, error);
+	});
 
-  // Handle errors, remove socket from bridge if it is exists
-  clientSocket.on('error', (error) => {
-    onError(clientSocket, error);
-  });
+	// Handle the connection end event, remove socket from bridge if it is exists
+	clientSocket.on("end", () => {
+		onEnd(clientSocket);
+	});
 
-  // Handle the connection end event, remove socket from bridge if it is exists
-  clientSocket.on('end', () => {
-    onEnd(clientSocket);
-  });
-
-  // When data is received from a client, decrypt and pass to processing
-  clientSocket.on('data', (data) => {
-    onData(clientSocket, data);
-  });
+	// When data is received from a client, decrypt and pass to processing
+	clientSocket.on("data", (data) => {
+		onData(clientSocket, data);
+	});
 });
 
-
 planeTcpServer.listen(serverPort, () => {
-  logger.info(`ELFIN IoT TCP proxy server is listening on port ${serverPort}`);
+	logger.info(`ELFIN IoT TCP proxy server is listening on port ${serverPort}`);
 });
